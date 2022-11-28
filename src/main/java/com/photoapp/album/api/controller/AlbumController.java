01056e4ed075e66dbf3f3891366ab3deb9a5b5ef
tanlh <huutanctct@gmail.com>
2022-11-29 00:08:52 +0700

    Add sleuth zipkin tracing

--- src/main/java/com/photoapp/album/api/controller/AlbumController.java
+++ src/main/java/com/photoapp/album/api/controller/AlbumController.java
@@ -4,6 +4,7 @@ import com.photoapp.album.api.model.AlbumResponse;
 import com.photoapp.album.mapper.AlbumMapper;
 import com.photoapp.album.service.AlbumService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users/{id}/albums")
 @RequiredArgsConstructor
+@Slf4j
 public class AlbumController {
 
     private final AlbumService albumService;
@@ -21,6 +23,7 @@ public class AlbumController {
 
     @GetMapping
     public List<AlbumResponse> userAlbums(@PathVariable String id) {
+        log.info("Calling get user albums");
         var albumEntities = albumService.getAlbums(id);
         var albumResponses = albumMapper.albumEntityToAlbumResponse(albumEntities);
 

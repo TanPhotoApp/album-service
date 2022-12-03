d79c2cccdfcb9819a8dcf67509cfb64e59c9f33d
tanlh <huutanctct@gmail.com>
2022-12-03 21:25:28 +0700

    Add security

---
+++ src/main/java/com/photoapp/album/security/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.photoapp.album.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .authorizeRequests()
+                .antMatchers("**/album/**", "/actuator/**").permitAll()
+                .anyRequest().denyAll()
+            .and()
+            .httpBasic();
+
+        return http.build();
+    }
+
+}
